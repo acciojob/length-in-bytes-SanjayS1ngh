079890cce1d0f614872185c20dcf65e6ed15c495
Sanjay Singh <114378504+SanjayS1ngh@users.noreply.github.com>
2026-07-24 07:27:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const byteSize = (str) => {
   // write your code here
+	if(str=="")return 0;
 	 return new Blob([str]).size;
 };
 
 // Do not change the code below
-// const str = prompt("Enter some string.");
+const str = prompt("Enter some string.");
 alert(byteSize(str));
